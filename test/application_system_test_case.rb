be503211c0eeb5236c082713c1d47dadfe491416
Moises Vargas <moisesweb@gmail.com>
2025-05-04 20:51:37 -0500

    fix rubocob issue

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -5,7 +5,7 @@ class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   # Use cuprite driver which is a headless Chrome driver that doesn't rely on
   # Selenium's automatic driver download mechanism
   Capybara.register_driver(:cuprite) do |app|
-    Capybara::Cuprite::Driver.new(app, window_size: [1400, 1400])
+    Capybara::Cuprite::Driver.new(app, window_size: [ 1400, 1400 ])
   end
 
   driven_by :cuprite
